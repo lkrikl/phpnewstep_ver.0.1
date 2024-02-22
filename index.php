5c9738a9c978ba71d06ae3cf82bd955dd8f0af3b
lkrikl <prosto2590@gmail.com>
2024-02-22 10:34:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>Тестируем PHP на прочность :)</title>
     </head>
     <body>
-        <?php echo '<p>Привет, мир! </p>'; ?>
+        <?php echo '<p>Привет, мир! Новый заход</p>'; ?>
     </body>
 </html>
