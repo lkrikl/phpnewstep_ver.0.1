2895dba55a0a15995fe437d22f571dbd00651e34
lkrikl <prosto2590@gmail.com>
2024-02-22 10:42:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,15 +11,13 @@
 			<p>Имя: <input type="text" name="name" /></p>
 			<p>Возраст: <input type="number" name="age" /></p>
 			<input type="submit" value="Отправить"><hr>
-		<?php
+<?php
 $name = "не определено";
 $age = "не определен";
 if(isset($_POST["name"])){
-  
     $name = $_POST["name"];
 }
 if(isset($_POST["age"])){
-  
     $age = $_POST["age"];
 }
 echo "Имя: $name <br> Возраст: $age";
