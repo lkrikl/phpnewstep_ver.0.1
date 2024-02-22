e2712f9223b78f238c13f079420e57aff57f3951
lkrikl <prosto2590@gmail.com>
2024-02-22 10:56:50 +0300

    Update index.php
    add lastname in form

--- index.php
+++ index.php
@@ -9,18 +9,23 @@
 		<h3>Форма ввода данных</h3>
 			<form action="index.php" method="POST">
 			<p>Имя: <input type="text" name="name" /></p>
+			<p>Фамилия: <input type="text" name="lastname"></p>
 			<p>Возраст: <input type="number" name="age" /></p>
 			<input type="submit" value="Отправить"><hr>
 <?php
 $name = "не определено";
+$lastname = "Не определено";
 $age = "не определен";
 if(isset($_POST["name"])){
     $name = $_POST["name"];
 }
+if(isset($_POST["lastname"])){
+	$lastname = $_POST["lastname"];
+}
 if(isset($_POST["age"])){
     $age = $_POST["age"];
 }
-echo "Имя: $name <br> Возраст: $age";
+echo "Имя: $name <br> Фамилия: $lastname <br> Возраст: $age";
 ?>
     </body>
 </html>
