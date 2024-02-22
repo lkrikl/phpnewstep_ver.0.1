1764842b10fc08ae6bea166a0b15583c2cfc2f78
lkrikl <prosto2590@gmail.com>
2024-02-22 10:41:28 +0300

    Update index.php
    New form name and age

--- index.php
+++ index.php
@@ -5,5 +5,26 @@
     </head>
     <body>
         <?php echo '<p>Привет, мир! Новый заход</p>'; ?>
+		
+		<h3>Форма ввода данных</h3>
+			<form action="index.php" method="POST">
+			<p>Имя: <input type="text" name="name" /></p>
+			<p>Возраст: <input type="number" name="age" /></p>
+			<input type="submit" value="Отправить"><hr>
+		<?php
+$name = "не определено";
+$age = "не определен";
+if(isset($_POST["name"])){
+  
+    $name = $_POST["name"];
+}
+if(isset($_POST["age"])){
+  
+    $age = $_POST["age"];
+}
+echo "Имя: $name <br> Возраст: $age";
+?>
+		
+		
     </body>
 </html>
